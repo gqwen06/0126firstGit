ed92973e12670ed583b18e7a84d136cc5436aedd
gqwen06 <6140wen@sina.com>
2021-01-26 16:53:55 +0800

    修改first.js 添加字符串

--- first.js
+++ first.js
@@ -6,3 +6,10 @@ let arr = [1,2,5,6,8];
 for(let j=0;j<arr.length;j++){
     document.write(arr[j]);
 }
+
+let str = "abc";
+let res = "";
+for(let k=0;k<str.length;k++){
+    res += str[k];
+}
+console.log(res);
